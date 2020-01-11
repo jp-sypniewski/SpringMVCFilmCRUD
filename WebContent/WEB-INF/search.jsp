86b705b24cb1b6d1405a52e0957d245fb71257f4
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-11 10:39:51 -0700

    init search landing

--- WebContent/WEB-INF/search.jsp
+++ WebContent/WEB-INF/search.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,17 @@
 <title>Film Query Site</title>
 </head>
 <body>
+
+<form action="search.do" method="GET">
+  <input type="text" name="filmId">
+  <input type="submit" value="Submit">
+</form>
+
+
+
+
+
+
 
 </body>
 </html>
--- src/main/java/com/skilldistillery/film/controller/FilmController.java
+++ src/main/java/com/skilldistillery/film/controller/FilmController.java
@@ -1,8 +1,20 @@
 package com.skilldistillery.film.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 
 @Controller
 public class FilmController {
+	
+	@Autowired
+	DatabaseAccessorObject dao = new DatabaseAccessorObject();
+	
+	@RequestMapping("search.do")
+	public String returnToSearch() {
+		return "WEB-INF/search.jsp";
+	}
 
 }
