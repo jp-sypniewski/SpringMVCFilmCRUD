3b80a2da5c9358c0e4f6f638b48363ba4bc7c1b6
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-11 10:55:30 -0700

    update web.xml and servlets

--- src/main/java/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/main/java/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -9,9 +9,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
+@Component
 public class DatabaseAccessorObject implements DatabaseAccessor {
 
 	private String user = "student";
